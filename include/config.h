bfd813fff41ab13985a06bc7dfd323be31f798e3
Eugeniy Marilev <marilev@jviba.com>
2015-03-20 00:55:04 +0200

    - commented JV_DB_PARAM_LOGGING

--- include/config.h
+++ include/config.h
@@ -11,6 +11,6 @@
 #define BOOST_FILESYSTEM_NO_DEPRECATED
 #define JVIBETTO_PATH "/home/djvibegga/projects/jvibetto/src"
 #define _UNICODE
-#define JV_DB_PARAM_LOGGING
+//#define JV_DB_PARAM_LOGGING
 
 #endif /* CONFIG_H_ */
